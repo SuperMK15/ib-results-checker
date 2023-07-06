02e218a7a312ea59ba097fa7cb8cfabebd8768dd
SuperMK15 <smartiskush@gmail.com>
2023-07-05 23:02:00 -0400

    updated main.py

--- main.py
+++ main.py
@@ -20,30 +20,50 @@ try:
     file_path = 'config.txt'
     config = read_config_file(file_path)
     personal_code = config.get('personal_code')
+    time_between_reloads = config.get('time_between_reloads')
     pin = config.get('pin')
     url = "https://candidates.ibo.org/#/Login"
 except:
     print("Invalid config.txt")
     exit()
 
-driver = webdriver.Chrome()
-driver.get(url)
+while True:
+    driver = webdriver.Chrome()
+    wait = WebDriverWait(driver, time_between_reloads)
+    driver.get(url)
 
-driver.find_element(By.ID, "personCode").send_keys(personal_code)
-driver.find_element(By.ID, "pinCode").send_keys(pin)
-print("Entered login data successfully")
+    try:
+        personCodeForm = wait.until(EC.presence_of_element_located((By.ID, "personCode")))
+        pinCodeForm = wait.until(EC.presence_of_element_located((By.ID, "pinCode")))
+        print("Login page has finished loading")
+        personCodeForm.send_keys(personal_code)
+        pinCodeForm.send_keys(pin)
+        print("Entered login data successfully")
+        login_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".ibo-btn-primary.login.mt-40")))
+        login_button.click()
+        print("Logged in successfully")
+    except:
+        print("Login page was too slow to load... Retrying from the start")
+        driver.quit()
+        continue
 
-wait = WebDriverWait(driver, 10)
-login_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".ibo-btn-primary.login.mt-40")))
-login_button.click()
-print("Logged in successfully")
+    try:
+        results_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".ibo-btn-primary.mt-4")))
+        results_button.click()
+        print("Navigated to results successfully")
+    except:
+        print("Results button was too slow to load... Retrying from the start")
+        driver.quit()
+        continue
 
-navigation_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".ibo-btn-primary.mt-4")))
-navigation_button.click()
-print("Navigated to results successfully")
-
-wait.until(EC.presence_of_element_located((By.TAG_NAME, "body")))
-print("Page has finished loading")
+    try:
+        wait.until(EC.presence_of_element_located((By.TAG_NAME, "body")))
+        print("Results page has finished loading")
+        break
+    except:
+        print("Results page was too slow to load... Retrying from the start")
+        driver.quit()
+        continue
 
 time.sleep(9999)
 driver.quit()
