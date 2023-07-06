b90f8ddbe9e33b064a32daf674665a9517e09fdc
SuperMK15 <smartiskush@gmail.com>
2023-07-05 21:44:09 -0400

    create git repo

---
+++ main.py
@@ -0,0 +1,51 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+def read_config_file(file_path):
+    config_data = {}
+    with open(file_path, 'r') as file:
+        for line in file:
+            line = line.strip()
+            if line:
+                key, value = line.split(':')
+                config_data[key.strip()] = value.strip()
+    return config_data
+
+
+try:
+    file_path = 'config.txt'
+    config = read_config_file(file_path)
+    username = config.get('username')
+    password = config.get('password')
+    time_between_reloads = int(config.get('time_between_reloads'))
+    url = "https://candidates.ibo.org/#/Login"
+except:
+    print("Invalid config.txt")
+    exit()
+
+while True:
+    driver = webdriver.Chrome()
+    driver.get(url)
+
+    driver.find_element(By.ID, "personCode").send_keys(username)
+    driver.find_element(By.ID, "pinCode").send_keys(password)
+    print("Entered login data successfully")
+
+    wait = WebDriverWait(driver, 10)
+    login_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".ibo-btn-primary.login.mt-40")))
+    login_button.click()
+    print("Logged in successfully")
+
+    navigation_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".ibo-btn-primary.mt-4")))
+    navigation_button.click()
+    print("Navigated to results successfully")
+
+    wait.until(EC.presence_of_element_located((By.TAG_NAME, "body")))
+    print("Page has finished loading")
+
+    time.sleep(time_between_reloads)
+    driver.quit()
